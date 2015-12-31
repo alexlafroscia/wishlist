70b02d20801c9c713d31dfe94f07d6ba172c1b5e
Alex LaFroscia <alex@lafroscia.com>
2015-12-31 14:45:13 -0500

    Add tests for session#login to prepare for TDD

--- frontend/tests/unit/services/session-test.js
+++ frontend/tests/unit/services/session-test.js
@@ -36,3 +36,67 @@ test('it clears the value if localStorage when the accessToken property is set t
   const valLocalStorage = localStorage.getItem('accessToken');
   assert.equal(undefined, valLocalStorage);
 });
+
+test('can get the login token by logging the user in', function(assert) {
+  assert.expect(2);
+  const service = this.subject({
+    accessToken: ''
+  });
+  return service.login('current-user@example.com', 'foobar')
+    .catch(function() {
+      assert.ok(false, 'Login failed');
+    })
+    .then(function() {
+      assert.ok(true, 'Login was successful');
+      assert.equal('abcde', service.get('accessToken'), 'Set the access token');
+    });
+});
+
+test('login returns a rejected promise if it failed', function(assert) {
+  // Set up error response for endpoint
+  server.get('session', { }, 401);
+
+  assert.expect(1);
+  const service = this.subject({
+    accessToken: ''
+  });
+  return service.login('current-user@example.com', 'foobar')
+    .then(function() {
+      assert.ok(false, 'Promise should have been rejected');
+    })
+    .catch(function() {
+      assert.ok(true, 'Promise was rejected');
+    });
+});
+
+test('login is rejected if the email is not present', function(assert) {
+  assert.expect(1);
+  const service = this.subject({
+    accessToken: ''
+  });
+  return service.login('', 'foobar')
+    .then(function() {
+      assert.ok(false, 'Promise should have been rejected');
+    })
+    .catch(function(data) {
+      const { errors } = data;
+      const [ error ] = errors;
+      assert.equal('Email must be provided', error);
+    });
+});
+
+test('login is rejected if the password is not present', function(assert) {
+  assert.expect(1);
+  const service = this.subject({
+    accessToken: ''
+  });
+  return service.login('current-user@example.com', '')
+    .then(function() {
+      assert.ok(false, 'Promise should have been rejected');
+    })
+    .catch(function(data) {
+      const { errors } = data;
+      const [ error ] = errors;
+      assert.equal('Password must be provided', error);
+    });
+});
