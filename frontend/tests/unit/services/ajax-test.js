2367fa2d6951bea05209e0f312183618e2ae517a
Alex LaFroscia <alex@lafroscia.com>
2016-01-04 14:42:18 -0500

    Switch to ember-cli-fake-server
    `ember-cli-fake-server` has a much nicer syntax than using Pretender by
    itself, and is much simpler than `ember-cli-mirage`, which seems to, at
    least at this point, but a little too complex for our needs.  This seems
    like to be a good middle ground between being easy to use and robust
    enough to make mocking requests simple.

--- frontend/tests/unit/services/ajax-test.js
+++ frontend/tests/unit/services/ajax-test.js
@@ -1,51 +1,47 @@
 import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
-import Pretender from 'pretender';
+import FakeServer, { stubRequest } from 'ember-cli-fake-server';
 
 const tokenRegex = /^Token{1}\s(\w*)$/;
 const authTokenValue = 'abcd';
 
 moduleFor('service:ajax', 'Unit | Service | ajax', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
+  setup() {
+    FakeServer.start();
+  },
+  teardown() {
+    FakeServer.stop();
+  }
 });
 
 test('it adds the access token if it is available', function(assert) {
   assert.expect(1);
 
-  const server = new Pretender(function() {
-    this.get('/api/session', function({ requestHeaders }) {
-      const match = tokenRegex.exec(requestHeaders.Authorization);
-      const [, authToken] = match;
-      assert.equal(authTokenValue, authToken, 'Added correct auth token');
-      return [200, {}, JSON.stringify({})];
-    });
+  stubRequest('get', '/api/session', function(request) {
+    const { requestHeaders } = request;
+    const match = tokenRegex.exec(requestHeaders.Authorization);
+    const [, authToken] = match;
+    assert.equal(authTokenValue, authToken, 'Added correct auth token');
+    request.ok();
   });
 
   const service = this.subject({
     session: Ember.Object.create({ accessToken: authTokenValue })
   });
-  return service.request('/api/session')
-    .then(function() {
-      server.shutdown();
-    });
+  return service.request('/api/session');
 });
 
 test('it does not add the auth header if the token is empty', function(assert) {
   assert.expect(1);
 
-  const server = new Pretender(function() {
-    this.get('/api/session', function({ requestHeaders }) {
-      assert.empty(requestHeaders.Authorization);
-      return [200, {}, JSON.stringify({})];
-    });
+  stubRequest('get', '/api/session', function(request) {
+    const { requestHeaders } = request;
+    assert.empty(requestHeaders.Authorization);
+    request.ok();
   });
 
   const service = this.subject({
     session: Ember.Object.create({ accessToken: '' })
   });
-  return service.request('/api/session')
-    .then(function() {
-      server.shutdown();
-    });
+  return service.request('/api/session');
 });
